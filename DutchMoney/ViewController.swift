79ee0056913ee5de69825730e0ad8c686c5e5bf0
Sarah <ysarah0503@gmail.com>
2022-01-18 03:14:02 +0900

    Initial Commit

---
+++ DutchMoney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DutchMoney
+//
+//  Created by 윤새라 on 2022/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
