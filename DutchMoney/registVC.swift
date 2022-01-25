2e9964a98af596f4dda096ee17af21ca4dbd2026
Sarah <ysarah0503@gmail.com>
2022-01-25 17:21:58 +0900

    [UPDATE] 회원가입 회원가입 성공

--- DutchMoney/registVC.swift
+++ DutchMoney/registVC.swift
@@ -15,16 +15,21 @@ class registVC: UIViewController {
     @IBOutlet weak var pwTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("HI")
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func temp(_ sender: UIButton) {
+        print("wow")
+    }
     
-
-    @IBAction func doneButtonTouched(_ sender: UIButton) {
+    @IBAction func doneButtonTouched(_ sender: Any) {
         Auth.auth().createUser(withEmail: emailTextField.text!, password: pwTextField.text!){
             (user,error) in
             if user != nil{
+                let sucessAlert = UIAlertController( title : "확인", message : "회원가입이 완료되었습니다 ", preferredStyle : UIAlertController.Style.alert)
+                let onAction = UIAlertAction(title : "확인" ,style: UIAlertAction.Style.default, handler: nil)
                 print("register success")
             }
             else{
@@ -33,10 +38,22 @@ class registVC: UIViewController {
         }
     }
     
-    
-    @IBAction func temp(_ sender: Any) {
-        print("click")
-    }
+//    @IBAction func doneButtonTouched(_ sender: UIButton) {
+//        Auth.auth().createUser(withEmail: emailTextField.text!, password: pwTextField.text!){
+//            (user,error) in
+//            if user != nil{
+//                print("register success")
+//            }
+//            else{
+//                print("register fail")
+//            }
+//        }
+//    }
+//    
+//    
+//    @IBAction func temp(_ sender: UIButton) {
+//        print("click")
+//    }
     
     /*
     // MARK: - Navigation
